7e879bf381a19249d3fbc07fcedbc40ecc7d1522
Aayush Mishra <aayushmishra4@gmail.com>
2025-09-12 18:12:29 +0530

    update: Update fetch news api url

--- Client/src/lib/apiConfig.ts
+++ Client/src/lib/apiConfig.ts
@@ -1,2 +1,2 @@
 export const API_BASE_URL = import.meta.env.VITE_BASE_URL || 'https://heavy-olympie-newsblitz-cea92358.koyeb.app';
-export const NEWS_DATA_API_URL = import.meta.env.VITE_NEWS_DATA_API_URL || 'http://127.0.0.1:5000';
+export const NEWS_DATA_API_URL = import.meta.env.VITE_NEWS_DATA_API_URL || 'https://ethnic-dormouse-newsblitzz-5d7d1d04.koyeb.app/';
