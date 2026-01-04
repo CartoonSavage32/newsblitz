7e1ed3f2a93e32ae888066466b52cc0f3519fb04
CartoonSavage32 <aayushmishra4@gmail.com>
2026-01-04 15:51:25 +0530

    attempt: Another attempt1

--- Client/next.config.js
+++ Client/next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  turbo: false,
   reactStrictMode: true,
   pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
   // Explicitly configure to avoid conflicts
